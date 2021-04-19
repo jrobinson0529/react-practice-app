ba66fd712aa52261e8baa1c7c52018bec6697375
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-04-19 10:09:54 -0500

    Setup files for reactjs

---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h2>INSIDE APP COMPONENT</h2>
+      <button className='btn btn-info'>I am a button</button>
+    </div>
+  );
+}
+
+export default App;
