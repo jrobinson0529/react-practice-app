5acf6bbb51dc7d24dd8954e39a235a9e57036d6d
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-04-20 10:20:36 -0500

    finished joke generator

--- src/App/index.js
+++ src/App/index.js
@@ -5,10 +5,11 @@ import getRandomJoke from '../data/jokeData';
 
 function App() {
   const [singleJoke, setSingleJoke] = useState({});
-  const [buttonState, setButtonState] = useState(false);
+  const [buttonState, setButtonState] = useState(0);
+  const [buttonText, setButtonText] = useState('Get Joke');
 
-  const toggleButtonState = () => {
-    setButtonState(!buttonState);
+  const toggleButtonState = (num) => {
+    setButtonState(num);
   };
   const getJoke = () => {
     getRandomJoke().then((response) => setSingleJoke(response));
@@ -16,24 +17,38 @@ function App() {
   useEffect(() => {
     getJoke();
   }, []);
-  console.warn(singleJoke);
   return (
     <div className='App'>
       <div className='joke-main-container'>
         <div className='image-container'></div>
         <div className='joke-setup-container'>
-          <h3 id='jokeSetup'>{buttonState ? singleJoke.setup : ''}</h3>
+          <h3 id='jokeSetup'>{buttonState === 1 || buttonState === 2 ? singleJoke.setup : ''}</h3>
         </div>
         <div className='joke-punchline-container'>
-          <h3 id='jokePunch'>{buttonState ? '' : singleJoke.punchline}</h3>
+          <h3 id='jokePunch'>{buttonState === 0 || buttonState === 1 ? '' : singleJoke.punchline}</h3>
         </div>
         <div className='btn-container'>
           <button className='btn btn-primary'
-            onClick={
-              toggleButtonState
-              }>
-            {buttonState ? 'Get PUNCH' : 'GET JOKE'}
-            </button>
+            onClick={() => {
+              switch (buttonState) {
+                case 0:
+                  toggleButtonState(1);
+                  setButtonText('Get Punchline');
+                  break;
+                case 1:
+                  setButtonText('Get New Joke');
+                  toggleButtonState(2);
+                  break;
+                case 2:
+                  setButtonText('Get Punchline');
+                  getJoke();
+                  toggleButtonState(1);
+                  break;
+                default:
+                  break;
+              }
+            }
+            }>{buttonText}</button>
     </div>
       </div>
     </div>
